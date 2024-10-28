ec1c552442555fcdb66e6243cecbb5976590dc11
KustovyKOAL <lesyavita@ya.ru>
2024-10-28 23:45:36 +0300

    Создаём функцию загружки иображения в метку

--- Dogs.py
+++ Dogs.py
@@ -1,8 +1,21 @@
 from tkinter import *
-import requests #для того чтобы загружать из интернета
-import PIL import Image, ImageTK #чтобы обрабатывать изображения
-from io import BytesIO #для обработки картинки
-
+import requests
+import PIL import Image, ImageTK
+from io import BytesIO
+from tkinter import messagebox as mb
+def show_image():
+    image_url = get_dog_image()
+    if image_url:
+        try:
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)#Загрузили изображение в двоичном коде
+            img = Image.open(img_data)
+            img.thumbnail((300,300))
+            label.config(image=img)
+            label.image = img
+        except Exception as e:
+            mb.showerror("Ошибка", f"Возникла ошибка {e}")
 
 window = Tk()
 window.title("Картинки с собачками")
